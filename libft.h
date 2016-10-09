9fa5a2e6c1e1a7483455f9bff2a42fb2fdd7f278
mo0k <julien.moucadel@gmail.com>
2016-10-09 23:35:02 +0200

    up

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmoucade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/07 22:52:04 by jmoucade          #+#    #+#             */
+/*   Updated: 2016/11/07 12:23:35 by jmoucade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef		LIBFT_H
+# define	LIBFT_H
+
+#include <unistd.h>
+
+void		ft_putchar(char c);
+void		ft_putstr(char *str);
+void		ft_putnbr(int nb);
+int			ft_strlen(char *str);
+
+#endif
--- mk_strlen.c
+++
@@ -1,9 +0,0 @@
-int			mk_strlen(char **str)
-{
-	int		i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
